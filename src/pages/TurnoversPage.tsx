1ba7d422f85fd226870f29cbca0ece491d8bc377
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-09-20 11:01:21 +0300

    Add navigation for turnovers

--- src/pages/TurnoversPage.tsx
+++ src/pages/TurnoversPage.tsx
@@ -1,16 +1,25 @@
-import {useLocation} from "react-router-dom";
+import {NavLink, useLocation} from "react-router-dom";
 import {PaymentsCell} from "../components/Payments/PaymentsCell";
 import {routes} from "../routes/Routes";
 import {TurnoversINSTCell} from "../components/Turnovers/TurnoversINST.Cell";
 import {TurnoversSCTCell} from "../components/Turnovers/TurnoversSCT.Cell";
 import {TurnoversSDDCell} from "../components/Turnovers/TurnoversSDD.Cell";
+import React from "react";
 
 export const TurnoversPage = () => {
     const location = useLocation();
 
+    let activeStyle = {
+        textDecoration: "underline",
+    };
+
     return (
         <div data-testid={'turnovers-page'}>
-            <PaymentsCell/>
+            <div>
+                <NavLink to={routes.turnovers.children.inst.path} style={({ isActive }) => isActive ? activeStyle : undefined}>INST</NavLink>&nbsp;/&nbsp;
+                <NavLink to={routes.turnovers.children.sct.path} style={({ isActive }) => isActive ? activeStyle : undefined}>SCT</NavLink>&nbsp;/&nbsp;
+                <NavLink to={routes.turnovers.children.sdd.path} style={({ isActive }) => isActive ? activeStyle : undefined}>SDD</NavLink>
+            </div>
             { location.pathname === routes.turnovers.path && <TurnoversINSTCell /> }
             { location.pathname === routes.turnovers.children.inst.fullPath && <TurnoversINSTCell /> }
             { location.pathname === routes.turnovers.children.sct.fullPath && <TurnoversSCTCell /> }
