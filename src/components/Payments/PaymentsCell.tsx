dbaed0cd9e052976be2e542da8be25e4c37f2e42
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-09-21 13:10:47 +0300

    Hide/Show navigation elements in PaymentsCell

--- src/components/Payments/PaymentsCell.tsx
+++ src/components/Payments/PaymentsCell.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {NavLink} from "react-router-dom";
 import {routes} from "../../routes/Routes";
+import {UserInBrowser} from "../Login/User.Slice";
+import {useAppSelector} from "../../app/hooks";
+import {PBX_Monitoring_SEPA_Infrastructure_Enum_BusinessArea} from "../../services/openapi";
 
 export const PaymentsCell = () => {
+
+    const user:UserInBrowser | null = useAppSelector(state => state.user.currentUser );
 
     let activeStyle = {
         textDecoration: "underline",
@@ -10,9 +15,28 @@ export const PaymentsCell = () => {
 
     return (
         <div>
-            <NavLink to={routes.payments.children.inst.path} style={({ isActive }) => isActive ? activeStyle : undefined}>INST</NavLink>&nbsp;/&nbsp;
-            <NavLink to={routes.payments.children.sct.path} style={({ isActive }) => isActive ? activeStyle : undefined}>SCT</NavLink>&nbsp;/&nbsp;
-            <NavLink to={routes.payments.children.sdd.path} style={({ isActive }) => isActive ? activeStyle : undefined}>SDD</NavLink>
+            { user &&
+                <>
+                    {
+                        user?.selectedCompany?.connections.find( connection => connection?.includes(PBX_Monitoring_SEPA_Infrastructure_Enum_BusinessArea.SEPA_INSTANT) ) &&
+                        <>
+                            <NavLink to={routes.payments.children.inst.path} style={({ isActive }) => isActive ? activeStyle : undefined}>INST</NavLink>&nbsp;/&nbsp;
+                        </>
+                    }
+                    {
+                        user?.selectedCompany?.connections.find( connection => connection?.includes(PBX_Monitoring_SEPA_Infrastructure_Enum_BusinessArea.SEPA_SCT) ) &&
+                        <>
+                            <NavLink to={routes.payments.children.sct.path} style={({ isActive }) => isActive ? activeStyle : undefined}>SCT</NavLink>&nbsp;/&nbsp;
+                        </>
+                    }
+                    {
+                        user?.selectedCompany?.connections.find( connection => connection?.includes(PBX_Monitoring_SEPA_Infrastructure_Enum_BusinessArea.SEPA_SDD) ) &&
+                        <>
+                            <NavLink to={routes.payments.children.sdd.path} style={({ isActive }) => isActive ? activeStyle : undefined}>SDD</NavLink>
+                        </>
+                    }
+                </>
+            }
         </div>
     );
 }
