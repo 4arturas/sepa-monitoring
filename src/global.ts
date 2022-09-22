d5c9d741130a5a992cac72a553d05852b7597a07
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-09-22 14:17:22 +0300

    Add getLastMonthFirsDay function into global.ts

--- src/global.ts
+++ src/global.ts
@@ -1,3 +1,5 @@
+import moment, {Moment} from "moment/moment";
+
 export const ROLE_ADMIN = 'Administrator';
 export const ROLE_NEED_TO_ASK_WHAT_ELSE_ROLES_ARE_AVAILABLE = '-----';
 
@@ -5,3 +7,9 @@ export const TABLE_PAGE_SIZE_DEFAULT = 5;
 
 
 export const MSG_BUSINESS_AREA_IS_NOT_SET = 'Business area is not set'
+
+export const getLastMonthFirstDay = (): Moment =>
+{
+    const lastMonth:Moment = moment().subtract(1, "month");
+    return lastMonth.startOf('month');
+}
