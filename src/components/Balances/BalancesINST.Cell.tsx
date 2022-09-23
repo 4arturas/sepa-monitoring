8ef4e5555d6115494a8d65236807db8b8c2e2629
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-09-23 08:37:07 +0300

    Implement search on BalancesINST

--- src/components/Balances/BalancesINST.Cell.tsx
+++ src/components/Balances/BalancesINST.Cell.tsx
@@ -4,11 +4,15 @@ import {
     Balance_Contracts_GenerateResponse,
     PBX_Monitoring_SEPA_Infrastructure_Enum_BusinessArea
 } from "../../services/openapi";
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 import {balancesSliceAction, BalancesSliceQuery} from "./Balances.Slice";
 import {UserInBrowser} from "../Login/User.Slice";
-import {Alert, Table} from "antd";
-import {MSG_BUSINESS_AREA_IS_NOT_SET, TABLE_PAGE_SIZE_DEFAULT} from "../../global";
+import {Button, DatePicker, Form, Input, Table} from "antd";
+import {
+    dateFormat_YYYY_MM_DD,
+    getLastMonthFirstDay,
+    TABLE_PAGE_SIZE_DEFAULT
+} from "../../global";
 import moment from "moment";
 
 const columns = [
@@ -60,6 +64,8 @@ const columns = [
 
 export const BalancesINSTCell = () => {
 
+    const { RangePicker } = DatePicker;
+
     const dispatch = useDispatch();
     const balances:Balance_Contracts_GenerateResponse | null    = useAppSelector( state => state.balances.inst );
     const loading:boolean                                       = useAppSelector( state => state.balances.loadingInst );
@@ -75,6 +81,30 @@ export const BalancesINSTCell = () => {
     }, [currentUser?.instIsSet] );
 
     return <div>
-        { balances && <Table dataSource={balances.items || []} columns={columns} pagination={{ pageSize: TABLE_PAGE_SIZE_DEFAULT }} rowKey={row=>`rowKey${rowCounter++}${row.account}`} loading={loading} bordered={true}/> }
+        <Form
+            data-testid='form-component'
+            name="basic"
+            layout={"inline"}
+            initialValues={{date: [getLastMonthFirstDay(), moment()]}}
+            onFinish={async (values: any) => {
+                const dateFrom:string = values.date[0].format(dateFormat_YYYY_MM_DD);
+                const dateTo:string = values.date[1].format(dateFormat_YYYY_MM_DD);
+                delete values['date'];
+                dispatch( balancesSliceAction.getBalancesInst( { companyId: currentUser?.userId, dateFrom: dateFrom, dateTo: dateTo,...values, businessArea: PBX_Monitoring_SEPA_Infrastructure_Enum_BusinessArea.SEPA_INSTANT } ) );
+            }}>
+            <Form.Item name="date">
+                <RangePicker format={dateFormat_YYYY_MM_DD} style={{width:'250px'}} />
+            </Form.Item>
+            <Form.Item name="account">
+                <Input placeholder="Account" style={{width:'200px'}}/>
+            </Form.Item>
+            <Form.Item style={{textAlign:'center'}}>
+                <Button htmlType="submit" type={"primary"}>Search</Button>&nbsp;
+                <Button htmlType="reset">Clear</Button>&nbsp;
+                <Button htmlType="submit" type={"primary"} onClick={() => alert('Not Implemented yet')}>Export</Button>
+            </Form.Item>
+        </Form>
+        <br/>
+        <Table dataSource={balances?.items || []} columns={columns} pagination={{ pageSize: TABLE_PAGE_SIZE_DEFAULT }} rowKey={row=>`rowKey${rowCounter++}${row.account}`} loading={loading} bordered={true}/>
     </div>;
 }
